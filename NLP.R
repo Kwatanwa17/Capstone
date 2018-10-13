67502dd6d6b50e4c5183ef3e56246b93898d9045
Kwatanwa17 <kwatanwa17@gmail.com>
2018-10-13 16:33:45 +0200

    predict function implementedgit add NLP.R

--- NLP.R
+++ NLP.R
@@ -1,9 +1,11 @@
-setwd("C:/Users/Keita/OneDrive - Universitat de Barcelona/DataScienceCapstone")
+memory.limit(size = 15000)
+setwd("C:/Users/Keita/Desktop/DataScienceCapstone")
 
 require(quanteda)
 require(readtext)
 require(data.table)
 require(dplyr)
+require(stringr)
 
 filelist <- list.files(path = "C:/Users/Keita/OneDrive - Universitat de Barcelona/DataScienceCapstone/en_US", pattern = ".*.txt")
 filenames <- gsub(pattern = "^en_US.", replacement = "", filelist)
@@ -16,7 +18,7 @@ sReadLines <- function(fnam) {
   iconv(res, "UTF-8", "ASCII", sub = "")
 }
 
-setwd("C:/Users/Keita/OneDrive - Universitat de Barcelona/DataScienceCapstone/en_US")
+setwd("C:/Users/Keita/Desktop/DataScienceCapstone/en_US")
 
 txtdata <- lapply(filelist, sReadLines)
 
@@ -34,12 +36,14 @@ txtdata <- lapply(filelist, sReadLines)
 merge_corpus <- lapply(txtdata, corpus)
 data_corpus <- merge_corpus[[1]] + merge_corpus[[2]] + merge_corpus[[3]]
 #Sampling the corpus. Use 1/10 of the data because of the CPU capacity.
+set.seed(12345)
 test_corpus <- corpus_sample(data_corpus, nrow(data_corpus$documents)/10)
 test_token_1gram <- tokens(test_corpus, remove_numbers = TRUE, remove_punct = TRUE,
-                     remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = FALSE)
+                     remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE)
 rm(txtdata)
 rm(merge_corpus)
 rm(data_corpus)
+rm(test_corpus)
 # 
 # DaMergeCorpus
 # blog_corpus <- corpus(blog_data)
@@ -90,10 +94,10 @@ rm(data_corpus)
 # en_US_5gram <- tokens_ngrams(en_US_token, n = 5)
 # en_US_6gram <- tokens_ngrams(en_US_token, n = 6)
 
-test_token_2gram <- tokens_ngrams(test_token, n = 2)
-test_token_3gram <- tokens_ngrams(test_token, n = 3)
-test_token_4gram <- tokens_ngrams(test_token, n = 4)
-test_token_5gram <- tokens_ngrams(test_token, n = 5)
+test_token_2gram <- tokens_ngrams(test_token_1gram, n = 2)
+test_token_3gram <- tokens_ngrams(test_token_1gram, n = 3)
+test_token_4gram <- tokens_ngrams(test_token_1gram, n = 4)
+test_token_5gram <- tokens_ngrams(test_token_1gram, n = 5)
 
 
 # #blog_dfm <- dfm(blog_corpus, stem = TRUE, remove = stopwords("english"), remove_punct = TRUE, remove_numbers = TRUE)
@@ -103,14 +107,15 @@ test_token_5gram <- tokens_ngrams(test_token, n = 5)
 # en_US_dfm <- dfm_remove(en_US_dfm, "rt")
 # en_US_dfm <- dfm_select(en_US_dfm, min_nchar = 2)
 
-test_dfm_1gram <- dfm(test_token)
+test_dfm_1gram <- dfm(test_token_1gram)
 test_dfm_2gram <- dfm(test_token_2gram)
 test_dfm_3gram <- dfm(test_token_3gram)
 test_dfm_4gram <- dfm(test_token_4gram)
 test_dfm_5gram <- dfm(test_token_5gram)
 
+
 rm(
-  list = c(test_token_1gram, test_token_2gram, test_token_3gram, test_token_4gram, test_token_5gram)
+  list = c("test_token_1gram", "test_token_2gram", "test_token_3gram", "test_token_4gram", "test_token_5gram")
 )
 
 test_freq_1gram <- textstat_frequency(test_dfm_1gram)
@@ -119,9 +124,107 @@ test_freq_3gram <- textstat_frequency(test_dfm_3gram)
 test_freq_4gram <- textstat_frequency(test_dfm_4gram)
 test_freq_5gram <- textstat_frequency(test_dfm_5gram)
 
-# rm(
-#   list = c(test_freq_1gram, test_freq_2gram, test_freq_3gram, test_freq_4gram, test_freq_5gram)
-# )
+rm(
+  list = c("test_dfm_1gram", "test_dfm_2gram", "test_dfm_3gram", "test_dfm_4gram", "test_dfm_5gram")
+)
+
+#Calculate the probability of each n-gram
+
+test_freq_1gram <- test_freq_1gram %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(test_freq_1gram[,2]))
+
+test_freq_2gram <- test_freq_2gram %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(test_freq_2gram[,2]))
+
+test_freq_3gram <- test_freq_3gram %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(test_freq_3gram[,2]))
+
+test_freq_4gram <- test_freq_4gram %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(test_freq_4gram[,2]))
+
+test_freq_5gram <- test_freq_5gram %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(test_freq_5gram[,2]))
+
+test_dt <- test_freq_5gram 
+test_dt_head <- head(test_dt,100)
+
+pred_sentence <- "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+
+cleanSentente <- function(sentence){
+  pred_token <- tokens(sentence, remove_numbers = TRUE, remove_punct = TRUE,
+                       remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE)
+  pred_token <- as.vector(unlist(pred_token))
+  final_4words <- pred_token[(length(pred_token)-3):length(pred_token)]
+  four <-  str_c(final_4words, collapse = "_")
+  three <-  str_c(final_4words[2:4], collapse = "_")
+  two <-  str_c(final_4words[3:4], collapse = "_")
+  one <-  final_4words[4]
+  c(four, three, two, one)
+}
+
+lookup <- cleanSentente(pred_sentence)
+
+sentence <- pred_sentence
+
+predNextWord <- function(sentence) {
+  lookup <- cleanSentente(sentence)
+  #Strating with 5gram
+  result <- subset(test_freq_5gram, grepl(paste("^", lookup[1], "_", sep =""), test_freq_5gram$feature))
+  if (max(result$frequency) > 10) {
+    result
+  } else {
+    #Check 4gram
+    result <- subset(test_freq_4gram, grepl(paste("^", lookup[2], "_", sep =""), test_freq_4gram$feature))
+    if (max(result$frequency) > 20) {
+      head(result,20)
+    } else {
+      #Check 3gram
+      result <- subset(test_freq_3gram, grepl(paste("^", lookup[3], "_", sep =""), test_freq_3gram$feature))
+      if (max(result$frequency) > 20) {
+        result
+      } else {
+        #check 2gram
+        result <- subset(test_freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), test_freq_2gram$feature))
+        if (max(result$frequency) > 20) {
+          result
+        } else {
+          print("Unseen word")
+        }
+      }
+    }
+  }
+}
+
+
+lookup <- subset(test_dt, grepl("^tsasa_end_of_the", test_dt$feature))
+nrow(lookup) == 0
+
+  
+
+
+
+# firstStirng <- function(string){
+#   string_vec = str_split(string, pattern = "_")
+#   first = str_c(string_vec[[1]][1:(length(string_vec[[1]])-1)], collapse = "_")
+#   first
+# }
+# 
+# lastStirng <- function(string){
+#   string_vec = str_split(string, pattern = "_")
+#   last = string_vec[[1]][length(string_vec[[1]])]
+#   last
+# }
+
 
 
 # 
