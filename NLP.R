e07f177814586f714000da8418ed4f71e1707257
Kwatanwa17 <kwatanwa17@gmail.com>
2018-10-16 23:39:11 +0200

    functions updated

--- NLP.R
+++ NLP.R
@@ -1,15 +1,17 @@
 memory.limit(size = 20000)
 setwd("C:/Users/Keita/Desktop/DataScienceCapstone")
 
+#Load packeges
 require(quanteda)
 require(readtext)
 require(data.table)
 require(dplyr)
 require(stringr)
 
+
 filelist <- list.files(path = "C:/Users/Keita/OneDrive - Universitat de Barcelona/DataScienceCapstone/en_US", pattern = ".*.txt")
-filenames <- gsub(pattern = "^en_US.", replacement = "", filelist)
-filenames <- gsub(pattern = ".txt$", replacement = "", filenames)
+# filenames <- gsub(pattern = "^en_US.", replacement = "", filelist)
+# filenames <- gsub(pattern = ".txt$", replacement = "", filenames)
 
 sReadLines <- function(fnam) {
   f <- file(fnam, "rb")
@@ -33,21 +35,84 @@ txtdata <- lapply(filelist, sReadLines)
 
 #Load as a List
 
-sampling_corpus <- function(x) {
-  set.seed(12345)
-  corpus_sample(x, nrow(x$documents)/10)
-}
+merge_txt <- c(txtdata[[1]], txtdata[[2]], txtdata[[3]])
 
-merge_corpus <- lapply(txtdata, corpus)
-test_corpus <- lapply(merge_corpus, sampling_corpus)
-test_corpus <- test_corpus[[1]] + test_corpus[[2]] + test_corpus[[3]]
+#Sampling a quarter part of the data set, witch subsequently will be divided traing and test data set.
+set.seed(12345)
+sample_txt <- sample(merge_txt, length(merge_txt)/4)
 
-#Sampling the corpus. Use 1/5 of the data because of the CPU capacity.
-test_token_1gram <- tokens(test_corpus, remove_numbers = TRUE, remove_punct = TRUE, remove_url = TRUE,
-                     remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE, remove_twitter = TRUE)
+sample_size <- floor(0.9 * length(sample_txt))
+
+set.seed(12345)
+train_ind <- sample(seq_len(length(sample_txt)), size = sample_size)
+train_txt <- sample_txt[train_ind]
+test_txt <- sample_txt[-train_ind]
+
+train_corpus <- corpus(train_txt)
+test_corpus <- corpus(test_txt)
+
 rm(txtdata)
-rm(merge_corpus)
-rm(test_corpus)
+rm(merge_txt)
+rm(sample_txt)
+rm(train_txt)
+rm(test_txt)
+
+#Craete tokens and calculate frqquency for each token
+freq_1gram <- tokens(train_corpus, ngrams = 1, remove_numbers = TRUE, remove_punct = TRUE, remove_url = TRUE,
+                      remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE, remove_twitter = TRUE) %>% 
+  dfm() %>% 
+  dfm_trim(min_termfreq = 2) %>% 
+  textstat_frequency() %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(frequency))
+
+freq_2gram <- tokens(train_corpus, ngrams = 2, remove_numbers = TRUE, remove_punct = TRUE, remove_url = TRUE,
+                      remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE, remove_twitter = TRUE) %>% 
+  dfm() %>% 
+  dfm_trim(min_termfreq = 2) %>% 
+  textstat_frequency() %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(frequency))
+
+freq_3gram <- tokens(train_corpus, ngrams = 3, remove_numbers = TRUE, remove_punct = TRUE, remove_url = TRUE,
+                     remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE, remove_twitter = TRUE) %>% 
+  dfm() %>% 
+  dfm_trim(min_termfreq = 2) %>% 
+  textstat_frequency() %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(frequency))
+
+freq_4gram <- tokens(train_corpus, ngrams = 4, remove_numbers = TRUE, remove_punct = TRUE, remove_url = TRUE,
+                      remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE, remove_twitter = TRUE) %>% 
+  dfm() %>% 
+  dfm_trim(min_termfreq = 2) %>% 
+  textstat_frequency() %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(frequency))
+  
+freq_5gram <- tokens(train_corpus, ngrams = 5, remove_numbers = TRUE, remove_punct = TRUE, remove_url = TRUE,
+                      remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE, remove_twitter = TRUE) %>% 
+  dfm() %>% 
+  dfm_trim(min_termfreq = 2) %>% 
+  textstat_frequency() %>% 
+  select(feature, frequency) %>% 
+  data.table() %>% 
+  mutate(prob = frequency/sum(frequency))
+
+load("freq_1gram.rda")
+
+# test_token_2gram <- tokens_ngrams(test_token_1gram, n = 2)
+# test_token_3gram <- tokens_ngrams(test_token_1gram, n = 3)
+# test_token_4gram <- tokens_ngrams(test_token_1gram, n = 4)
+# test_token_5gram <- tokens_ngrams(test_token_1gram, n = 5)
+
+
+rm(train_corpus)
+
 # 
 # DaMergeCorpus
 # blog_corpus <- corpus(blog_data)
@@ -98,10 +163,10 @@ rm(test_corpus)
 # en_US_5gram <- tokens_ngrams(en_US_token, n = 5)
 # en_US_6gram <- tokens_ngrams(en_US_token, n = 6)
 
-test_token_2gram <- tokens_ngrams(test_token_1gram, n = 2)
-test_token_3gram <- tokens_ngrams(test_token_1gram, n = 3)
-test_token_4gram <- tokens_ngrams(test_token_1gram, n = 4)
-test_token_5gram <- tokens_ngrams(test_token_1gram, n = 5)
+# test_token_2gram <- tokens_ngrams(test_token_1gram, n = 2)
+# test_token_3gram <- tokens_ngrams(test_token_1gram, n = 3)
+# test_token_4gram <- tokens_ngrams(test_token_1gram, n = 4)
+# test_token_5gram <- tokens_ngrams(test_token_1gram, n = 5)
 
 
 # #blog_dfm <- dfm(blog_corpus, stem = TRUE, remove = stopwords("english"), remove_punct = TRUE, remove_numbers = TRUE)
@@ -134,105 +199,303 @@ test_token_5gram <- tokens_ngrams(test_token_1gram, n = 5)
 
 #Calculate the probability of each n-gram
 
-test_freq_1gram <-dfm(test_token_1gram) %>% 
-  dfm_trim(min_termfreq = 2) %>% 
-  textstat_frequency() %>% 
-  select(feature, frequency) %>% 
-  data.table() %>% 
-  mutate(prob = frequency/sum(frequency))
+# freq_1gram <-dfm(token_1gram) %>% 
+#   dfm_trim(min_termfreq = 2) %>% 
+#   textstat_frequency() %>% 
+#   select(feature, frequency) %>% 
+#   data.table() %>% 
+#   mutate(prob = frequency/sum(frequency))
+# 
+# freq_2gram <-dfm(token_2gram) %>% 
+#   dfm_trim(min_termfreq = 2) %>% 
+#   textstat_frequency() %>% 
+#   select(feature, frequency) %>% 
+#   data.table() %>% 
+#   mutate(prob = frequency/sum(frequency))
+# 
+# freq_3gram <-dfm(token_3gram) %>% 
+#   dfm_trim(min_termfreq = 2) %>% 
+#   textstat_frequency() %>% 
+#   select(feature, frequency) %>% 
+#   data.table() %>% 
+#   mutate(prob = frequency/sum(frequency))
+# 
+# freq_4gram <-dfm(token_4gram) %>% 
+#   dfm_trim(min_termfreq = 2) %>% 
+#   textstat_frequency() %>% 
+#   select(feature, frequency) %>% 
+#   data.table() %>% 
+#   mutate(prob = frequency/sum(frequency))
+# 
+# freq_5gram <-dfm(token_5gram) %>% 
+#   dfm_trim(min_termfreq = 2) %>% 
+#   textstat_frequency() %>% 
+#   select(feature, frequency) %>% 
+#   data.table() %>% 
+#   mutate(prob = frequency/sum(frequency))
 
-test_freq_2gram <-dfm(test_token_2gram) %>% 
-  dfm_trim(min_termfreq = 2) %>% 
-  textstat_frequency() %>% 
-  select(feature, frequency) %>% 
-  data.table() %>% 
-  mutate(prob = frequency/sum(frequency))
+# cleanSentence <- function(sentence){
+#   pred_token <- tokens(sentence, remove_numbers = TRUE, remove_punct = TRUE,
+#                        remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE)
+#   pred_token <- tokens_tolower(pred_token)
+#   pred_token <- as.vector(unlist(pred_token))
+#   if (length(pred_token) > 3) {
+#   final_4words <- pred_token[(length(pred_token)-3):length(pred_token)]
+#   
+#   four <-  str_c(final_4words, collapse = "_")
+#   three <-  str_c(final_4words[2:4], collapse = "_")
+#   two <-  str_c(final_4words[3:4], collapse = "_")
+#   one <-  final_4words[4]
+#   c(four, three, two, one)
+#   } else c(NULL, NULL, NULL, NULL)
+# }
 
-test_freq_3gram <-dfm(test_token_3gram) %>% 
-  dfm_trim(min_termfreq = 2) %>% 
-  textstat_frequency() %>% 
-  select(feature, frequency) %>% 
-  data.table() %>% 
-  mutate(prob = frequency/sum(frequency))
+cleanSentenceUpdated <- function(sentence){
+  pred_token <- tokens(sentence, remove_numbers = TRUE, remove_punct = TRUE,
+                       remove_symbols = TRUE, remove_separators = TRUE,
+                       remove_twitter = TRUE, remove_hyphens = TRUE, remove_url = TRUE)
+  pred_token <- tokens_tolower(pred_token)
+  pred_token <- as.vector(unlist(pred_token))
+  
+  #Check if the length is al least 4
+  if (length(pred_token) > 3) {
+    final_words <- pred_token[(length(pred_token)-3):length(pred_token)]
+    #Check if the length is al least 3
+  } else if (length(pred_token) > 2) {
+    final_words <- pred_token[(length(pred_token)-2):length(pred_token)]
+    #Check if the length is al least 2
+  } else if (length(pred_token) > 1) {
+    final_words <- pred_token[(length(pred_token)-1):length(pred_token)]
+    #Otherwise the length is 1
+  } else {
+    final_words <- pred_token
+  }
+  
+  if (length(final_words) == 4) {
+    four <-  str_c(final_words, collapse = "_")
+    three <-  str_c(final_words[2:4], collapse = "_")
+    two <-  str_c(final_words[3:4], collapse = "_")
+    one <-  final_words[4]
+    output <- c(four, three, two, one)
+  } else if (length(final_words) == 3) {
+    three <-  str_c(final_words, collapse = "_")
+    two <-  str_c(final_words[2:3], collapse = "_")
+    one <-  final_words[3]
+    output <- c(NA, three, two, one)
+  } else if (length(final_words) == 2) {
+    two <-  str_c(final_words, collapse = "_")
+    one <-  final_words[2]
+    output <- c(NA, NA, two, one)
+  } else {
+    one <- final_words
+    output <- c(NA, NA, NA, one)
+  }
+  
+  output
+  
+}
+  
 
-test_freq_4gram <-dfm(test_token_4gram) %>% 
-  dfm_trim(min_termfreq = 2) %>% 
-  textstat_frequency() %>% 
-  select(feature, frequency) %>% 
-  data.table() %>% 
-  mutate(prob = frequency/sum(frequency))
-
-test_freq_5gram <-dfm(test_token_5gram) %>% 
-  dfm_trim(min_termfreq = 2) %>% 
-  textstat_frequency() %>% 
-  select(feature, frequency) %>% 
-  data.table() %>% 
-  mutate(prob = frequency/sum(frequency))
-
-cleanSentente <- function(sentence){
+sepSentence <- function(sentence){
   pred_token <- tokens(sentence, remove_numbers = TRUE, remove_punct = TRUE,
                        remove_symbols = TRUE, remove_separators = TRUE, remove_hyphens = TRUE)
   pred_token <- tokens_tolower(pred_token)
   pred_token <- as.vector(unlist(pred_token))
-  final_4words <- pred_token[(length(pred_token)-3):length(pred_token)]
-  four <-  str_c(final_4words, collapse = "_")
-  three <-  str_c(final_4words[2:4], collapse = "_")
-  two <-  str_c(final_4words[3:4], collapse = "_")
-  one <-  final_4words[4]
-  c(four, three, two, one)
+  first <- str_c(pred_token[1:length(pred_token)-1], collapse = " ")
+  final <- pred_token[length(pred_token)]
+  c(first, final)
 }
 
-predNextWord <- function(sentence) {
-  lookup <- cleanSentente(sentence)
-  #Strating with 5gram
-  result <- subset(test_freq_5gram, grepl(paste("^", lookup[1], "_", sep =""), test_freq_5gram$feature))
-  if (max(result$frequency) > 30) {
-    result
-  } else {
-    #Check 4gram
-    result <- subset(test_freq_4gram, grepl(paste("^", lookup[2], "_", sep =""), test_freq_4gram$feature))
-    if (max(result$frequency) > 30) {
-      head(result,20)
+#CREATE TWO BACK MODEL
+#1 SIMPLE ONE
+#2 ALFA MODEL ALPHA = 0.5??
+
+#else if!
+sentence <- test_sentence
+#1rm(fr)
+# predNextWord <- function(sentence) {
+#   lookup <- cleanSentence(sentence)
+#   #Strating with 5gram
+#   result_5gram <- subset(freq_5gram, grepl(paste("^", lookup[1], "_", sep =""), freq_5gram$feature))
+#   if (nrow(result_5gram) != 0) {
+#     result <- str_split(result_5gram[, 1][1], pattern = "_")
+#     result <- as.vector(result[[1]])
+#     result[5]
+#   } else {
+#     #Check 4gram
+#     result_4gram <- subset(freq_4gram, grepl(paste("^", lookup[2], "_", sep =""), freq_4gram$feature))
+#     if (nrow(result_4gram) != 0) {
+#       result <- str_split(result_4gram[, 1][1], pattern = "_")
+#       result <- as.vector(result[[1]])
+#       result[4]
+#     } else {
+#       #Check 3gram
+#       result_3gram <- subset(freq_3gram, grepl(paste("^", lookup[3], "_", sep =""), freq_3gram$feature))
+#       if (nrow(result_3gram) != 0) {
+#         result <- str_split(result_3gram[, 1][1], pattern = "_")
+#         result <- as.vector(result[[1]])
+#         result[3]
+#       } else {
+#         #check 2gram
+#         result_2gram <- subset(freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), freq_2gram$feature))
+#         if (nrow(result_2gram) != 0) {
+#           result <- str_split(result_2gram[, 1][1], pattern = "_")
+#           result <- as.vector(result[[1]])
+#           result[2]
+#         } else {
+#           freq_1gram[, 1][1]
+#         }
+#       }
+#     }
+#   }
+# }
+
+
+predNextWordUpdated <- function(sentence) {
+  lookup <- cleanSentenceUpdated(sentence)
+  
+  #Check if the length of word is at least 4
+  if (is.na(lookup[1]) == FALSE) {
+    result_5gram <- subset(freq_5gram, grepl(paste("^", lookup[1], "_", sep =""), freq_5gram$feature))
+    if (nrow(result_5gram) != 0) {
+      result <- str_split(result_5gram[, 1][1], pattern = "_")
+      result <- as.vector(result[[1]])
+      result[5]
     } else {
-      #Check 3gram
-      result <- subset(test_freq_3gram, grepl(paste("^", lookup[3], "_", sep =""), test_freq_3gram$feature))
-      if (max(result$frequency) > 100) {
-        result
+      #Check 4gram
+      result_4gram <- subset(freq_4gram, grepl(paste("^", lookup[2], "_", sep =""), freq_4gram$feature))
+      if (nrow(result_4gram) != 0) {
+        result <- str_split(result_4gram[, 1][1], pattern = "_")
+        result <- as.vector(result[[1]])
+        result[4]
       } else {
-        #check 2gram
-        result <- subset(test_freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), test_freq_2gram$feature))
-        if (max(result$frequency) > 100) {
-          result
+        #Check 3gram
+        result_3gram <- subset(freq_3gram, grepl(paste("^", lookup[3], "_", sep =""), freq_3gram$feature))
+        if (nrow(result_3gram) != 0) {
+          result <- str_split(result_3gram[, 1][1], pattern = "_")
+          result <- as.vector(result[[1]])
+          result[3]
         } else {
-          head(test_freq_1gram,100)
+          #check 2gram
+          result_2gram <- subset(freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), freq_2gram$feature))
+          if (nrow(result_2gram) != 0) {
+            result <- str_split(result_2gram[, 1][1], pattern = "_")
+            result <- as.vector(result[[1]])
+            result[2]
+          } else {
+            freq_1gram[, 1][1]
+          }
         }
       }
     }
+    #Check if the length of word is at least 3
+  } else if (is.na(lookup[2]) == FALSE) {
+    
+    #Check 4gram
+    result_4gram <- subset(freq_4gram, grepl(paste("^", lookup[2], "_", sep =""), freq_4gram$feature))
+    if (nrow(result_4gram) != 0) {
+      result <- str_split(result_4gram[, 1][1], pattern = "_")
+      result <- as.vector(result[[1]])
+      result[4]
+    } else {
+      #Check 3gram
+      result_3gram <- subset(freq_3gram, grepl(paste("^", lookup[3], "_", sep =""), freq_3gram$feature))
+      if (nrow(result_3gram) != 0) {
+        result <- str_split(result_3gram[, 1][1], pattern = "_")
+        result <- as.vector(result[[1]])
+        result[3]
+      } else {
+        #check 2gram
+        result_2gram <- subset(freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), freq_2gram$feature))
+        if (nrow(result_2gram) != 0) {
+          result <- str_split(result_2gram[, 1][1], pattern = "_")
+          result <- as.vector(result[[1]])
+          result[2]
+        } else {
+          freq_1gram[, 1][1]
+        }
+      }
+    }
+    
+    #Check if the length of word is at least 2
+  } else if (is.na(lookup[3]) == FALSE) { 
+    
+    #Check 3gram
+    result_3gram <- subset(freq_3gram, grepl(paste("^", lookup[3], "_", sep =""), freq_3gram$feature))
+    if (nrow(result_3gram) != 0) {
+      result <- str_split(result_3gram[, 1][1], pattern = "_")
+      result <- as.vector(result[[1]])
+      result[3]
+    } else {
+      #check 2gram
+      result_2gram <- subset(freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), freq_2gram$feature))
+      if (nrow(result_2gram) != 0) {
+        result <- str_split(result_2gram[, 1][1], pattern = "_")
+        result <- as.vector(result[[1]])
+        result[2]
+      } else {
+        freq_1gram[, 1][1]
+      }
+    }
+    
+  } else {
+    
+    #Otherwise word length is 1
+    #check 2gram
+    result_2gram <- subset(freq_2gram, grepl(paste("^", lookup[4], "_", sep =""), freq_2gram$feature))
+    if (nrow(result_2gram) != 0) {
+      result <- str_split(result_2gram[, 1][1], pattern = "_")
+      result <- as.vector(result[[1]])
+      result[2]
+    } else {
+      freq_1gram[, 1][1]
+    }
+    
+    
   }
-}
-
-checkWord <- function(df, word) {
-  subset(df, grepl(word, df$feature))
-}
-   
-
-test_sentence <- "I like how the same people are in almost all of Adam Sandler's"
-test_df <- predNextWord(test_sentence)
-
-test_words <- c("novels","stories", "pictures", "movies")
-
-for (x in 1:length(test_words)) {
-  result = checkWord(test_df, test_words[x])
-  print(test_words[x])
-  print(result)
-}
-
-for (x in 1:length(test_words)) {
-  result = checkWord(test_freq_1gram, test_words[x])
-  print(test_words[x])
-  print(result)
   
 }
+
+
+#2
+
+
+
+# checkWord <- function(df, word) {
+#   subset(df, grepl(paste(word, "$", sep =""), df$feature))
+# }
+
+test_sentence <- "Ifm thankful my childhood was filled with imagination and bruises from playing"
+predNextWord(test_sentence)
+
+test_sentences <- apply(test_corpus$documents, 1, sepSentence)
+a <- head(test_sentences, 5)
+
+b <- unlist(test_sentences)
+first_index <- seq(1, length(b), 2)
+
+first <- b[first_index]
+last <- b[-first_index]
+
+set.seed(7777)
+sample_index <- sample(1:length(first), 1000)
+
+pred_word <- sapply(first[sample_index], predNextWordUpdated)
+
+mean(pred_word == last[sample_index])
+# 
+# for (x in 1:length(test_words)) {
+#   result = checkWord(test_df, test_words[x])
+#   print(test_words[x])
+#   print(result)
+# }
+# 
+# for (x in 1:length(test_words)) {
+#   result = checkWord(freq_1gram, test_words[x])
+#   print(test_words[x])
+#   print(result)
+#   
+# }
 
 
 # firstStirng <- function(string){
@@ -288,3 +551,4 @@ for (x in 1:length(test_words)) {
 #Topfeatures
 #
 #fcm textplot_network
+
